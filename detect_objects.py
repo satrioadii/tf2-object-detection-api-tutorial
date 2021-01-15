6f30e0c02b182dfa0b500ba53cd4293a7f5da93a
satrioadii <satriooadi@gmail.com>
2021-01-15 22:30:56 +0700

    Update: Add Auto Write Result Log

--- detect_objects.py
+++ detect_objects.py
@@ -5,34 +5,10 @@ import argparse
 
 from detector import DetectorTF2
 
-
-def DetectFromVideo(detector, Video_path, save_output=False, output_dir='output/'):
-
-	cap = cv2.VideoCapture(Video_path)
-	if save_output:
-		output_path = os.path.join(output_dir, 'detection_'+ Video_path.split("/")[-1])
-		frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-		frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-		out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), 30, (frame_width, frame_height))
-
-	while (cap.isOpened()):
-		ret, img = cap.read()
-		if not ret: break
-
-		timestamp1 = time.time()
-		det_boxes = detector.DetectFromImage(img)
-		elapsed_time = round((time.time() - timestamp1) * 1000) #ms
-		img = detector.DisplayDetections(img, det_boxes, det_time=elapsed_time)
-
-		cv2.imshow('TF2 Detection', img)
-		if cv2.waitKey(1) == 27: break
-
-		if save_output:
-			out.write(img)
-
-	cap.release()
-	if save_output:
-		out.release()
+def WriteFile(output_dir, file_name, content):
+	f = open(output_dir + '/' + file_name, 'a+')
+	f.write(content)
+	f.close()
 
 
 def DetectImagesFromFolder(detector, images_dir, save_output=False, output_dir='output/'):
@@ -47,13 +23,12 @@ def DetectImagesFromFolder(detector, images_dir, save_output=False, output_dir='
 			elapsed_time = round((time.time() - timestamp1) * 1000) #ms
 			print('Elapsed Time:', str(elapsed_time))
 			img = detector.DisplayDetections(img, det_boxes)
-
-			# cv2.imshow('TF2 Detection', img)
-			# cv2.waitKey(0)
+			text_to_save = str(image_path) + ': ' + str(len(det_boxes)) + '[' + str(elapsed_time/1000) + 's] ]r]n' 
 
 			if save_output:
 				img_out = os.path.join(output_dir, file.name)
 				cv2.imwrite(img_out, img)
+				WriteFile(img_out, 'ResultLog.txt', text_to_save)
 
 
 if __name__ == "__main__":
@@ -67,9 +42,7 @@ if __name__ == "__main__":
 	                    type=str, default=None) # example input "1,3" to detect person and car
 	parser.add_argument('--threshold', help='Detection Threshold', type=float, default=0.4)
 	parser.add_argument('--images_dir', help='Directory to input images)', default='data/samples/images/')
-	parser.add_argument('--video_path', help='Path to input video)', default='data/samples/pedestrian_test.mp4')
 	parser.add_argument('--output_directory', help='Path to output images and video', default='data/samples/output')
-	parser.add_argument('--video_input', help='Flag for video input, default: False', action='store_true')  # default is false
 	parser.add_argument('--save_output', help='Flag for save images and video with detections visualized, default: False',
 	                    action='store_true')  # default is false
 	args = parser.parse_args()
@@ -85,10 +58,6 @@ if __name__ == "__main__":
 	# instance of the class DetectorTF2
 	detector = DetectorTF2(args.model_path, args.path_to_labelmap, class_id=id_list, threshold=args.threshold)
 
-	if args.video_input:
-		DetectFromVideo(detector, args.video_path, save_output=args.save_output, output_dir=args.output_directory)
-	else:
-		DetectImagesFromFolder(detector, args.images_dir, save_output=args.save_output, output_dir=args.output_directory)
+	DetectImagesFromFolder(detector, args.images_dir, save_output=args.save_output, output_dir=args.output_directory)
 
 	print("Done ...")
-	cv2.destroyAllWindows()
