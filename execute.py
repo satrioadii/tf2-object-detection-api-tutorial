bfb8ab1765614fbcb8b7cc6d4f4abc3c535d9bb4
satrioadii <satrio@nichestack.io>
2021-07-13 22:24:31 +0700

    adjust execute.py

--- execute.py
+++ execute.py
@@ -1,16 +1,16 @@
 import subprocess
 
 models = [
-    # 'faster-rcnn-resnet50-6000',
+    'faster-rcnn-resnet50-6000',
     # 'faster-rcnn-resnet50-5000',
     # 'faster-rcnn-resnet50-4000',
-    'ssd-mobilenet-v2-4000',
+    # 'ssd-mobilenet-v2-4000',
     # 'ssd-mobilenet-v2-6000',
     # 'ssd-mobilenet-v2-5000',
 ]
 
 threshold_setup = [0.3]
-test_images_folders = ['PL_5/1', 'PL_5/2', 'PL_5/3', 'PL_8/1', 'PL_8/2', 'PL_8/3']
+test_images_folders = ['1', '2']
 
 for threshold in threshold_setup:
     
