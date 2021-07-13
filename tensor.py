002cafb3e014120a3ad0119b132a21e12e06f674
satrioadii <satrio@nichestack.io>
2021-07-13 21:20:13 +0700

    format document

--- tensor.py
+++ tensor.py
@@ -3,34 +3,35 @@ import numpy as np
 from PIL import Image
 import time
 
+
 def make_image_square(filename):
-	img = cv2.imread(filename)
-	#Size of the image
-	# s = max(img.shape[0:2])
-	s = 640
+    img = cv2.imread(filename)
+    # Size of the image
+    # s = max(img.shape[0:2])
+    s = 640
 
-	#Creating a dark square with NUMPY  
-	f = np.zeros((s,s,3),np.uint8)
+    # Creating a dark square with NUMPY
+    f = np.zeros((s, s, 3), np.uint8)
 
-	#Getting the centering position
-	ax,ay = (s - img.shape[1])//2,(s - img.shape[0])//2
-	
-	#Pasting the 'image' in a centering position
-	f[ay:img.shape[0]+ay,ax:ax+img.shape[1]] = img
-	cv2.imwrite(filename,f)
+    # Getting the centering position
+    ax, ay = (s - img.shape[1])//2, (s - img.shape[0])//2
+
+    # Pasting the 'image' in a centering position
+    f[ay:img.shape[0]+ay, ax:ax+img.shape[1]] = img
+    cv2.imwrite(filename, f)
 
 
 def crop_image():
     for image_index in range(2):
         folder_name = 'data/'
-	    image_name = folder_name + str(image_index + 1) + '.jpg'
+        image_name = folder_name + str(image_index + 1) + '.jpg'
         img = cv2.imread(image_name)
 
         h, w, c = img.shape
 
-        print(h,w, image_name)
+        print(h, w, image_name)
 
-        w_constant  = w/3
+        w_constant = w/3
         h_constant = h/2
 
         image_part_index = 0
@@ -51,7 +52,8 @@ def crop_image():
 
                 # For testing image set
                 section_name = str(image_index+1) + '/'
-                file_name = folder_name + section_name + str(image_part_index+1) + '.jpg'
+                file_name = folder_name + section_name + \
+                    str(image_part_index+1) + '.jpg'
 
                 crop_img = img[start_height:end_height, start_width:end_width]
 
@@ -61,20 +63,18 @@ def crop_image():
                 make_image_square(file_name)
 
 
-
 def main():
 
-    ratarata=0
+    ratarata = 0
     #=========== Put your Program in here ========#
     crop_image()
     time.sleep(5)
     #====== Put the return value to ratarata =====#
-    ratarata=200
+    ratarata = 200
     #=============================================#
 
     print(ratarata)
 
+
 if __name__ == "__main__":
     main()
-
-
